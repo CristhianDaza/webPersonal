e4f5e210c0e03425ff9861d245bff21096bcc7ac
Cristhian Daza <cristian.d.b@hotmail.com>
2020-05-01 16:15:07 -0500

    Instalando Pug y limpiando el proyecto

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,9 +7,9 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
-      options: {
-        customProperties: true,
-      },
+    options: {
+      customProperties: true,
+    },
     themes: {
       light: {
         primary: '#ee44aa',
@@ -18,12 +18,12 @@ export default new Vuetify({
         error: '#FF5252',
         info: '#2196F3',
         success: '#4CAF50',
-        warning: '#FFC107'
+        warning: '#FFC107',
       },
     },
   },
-    lang: {
-      locales: { es },
-      current: 'es',
-    },
+  lang: {
+    locales: { es },
+    current: 'es',
+  },
 });
---
+++ src/views/Inicio.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div
+    h1  Desde Inicio
+</template>
+
+<script>
+export default {
+  name: 'Inicio',
+  components: {},
+};
+</script>
